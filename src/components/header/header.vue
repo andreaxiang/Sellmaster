0daec5b92aff1b94f83ec0beaf379a93a8bff9bf
andreaxiang <xiangxue89@163.com>
2017-12-16 23:23:11 +0800

    project init settings

---
+++ src/components/header/header.vue
@@ -0,0 +1,19 @@
+<template>
+   <div id="App">
+     I am root element
+   </div>
+ </template>
+ 
+ <script>
+ 
+ export default {
+   name: '',
+   data: function(){
+    
+   }
+ }
+ </script>
+ 
+ <style>
+  
+ </style>
